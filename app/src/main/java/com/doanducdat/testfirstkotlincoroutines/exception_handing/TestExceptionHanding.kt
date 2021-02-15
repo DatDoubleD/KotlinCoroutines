932589dd0b77db89c4246c463899038be5024eac
DatDoubleD <buonqa05@gmail.com>
2021-02-15 13:01:51 +0700

    Upload

--- app/src/main/java/com/doanducdat/testfirstkotlincoroutines/exception_handing/TestExceptionHanding.kt
+++ app/src/main/java/com/doanducdat/testfirstkotlincoroutines/exception_handing/TestExceptionHanding.kt
@@ -1,11 +1,13 @@
 package com.doanducdat.testfirstkotlincoroutines.exception_handing
 
 import kotlinx.coroutines.*
+import java.lang.ArithmeticException
 import java.lang.Exception
 import java.lang.IndexOutOfBoundsException
 import java.lang.NullPointerException
+import java.util.logging.Handler
 
-fun main() {
+fun c1andc2(){
     runBlocking {
         val job = GlobalScope.launch {
             try {
@@ -22,7 +24,92 @@ fun main() {
         }
         //job 1 trả vế 1 deferred
         // lấy giá trị = await(), lúc này lỗi mới dc ném ra
-        job1.await()
-
+        try{
+            job1.await()
+        }catch (ex: IndexOutOfBoundsException){
+            println(ex.toString())
+        }
     }
+}
+
+fun c3(){
+    //launch -> dung handler
+    //async -> k the dung launch -> phai dung trycatch
+    runBlocking {
+        val handler = CoroutineExceptionHandler{_, exception ->
+            println("Error here, ${exception.toString()}")
+        }
+        val job:Job = GlobalScope.launch (handler + Dispatchers.Default){
+            println("Throw exception from launch")
+            throw NullPointerException()
+        }
+        job.join()
+    }
+}
+
+fun c3vsMultiException() {
+    runBlocking {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            println("Error here, ${exception.toString()}, exception was suppressed: ${exception.suppressed.contentToString()}")
+        }
+        var job: Job = GlobalScope.launch(handler) {
+            launch {
+                println("1")
+                delay(300)
+                throw NullPointerException("Error 1")
+            }
+            launch {
+                println("2")
+                delay(300)
+                throw ArithmeticException("Error 2")
+
+            }
+            launch {
+                println("3")
+                delay(300)
+                throw IndexOutOfBoundsException("Error 3")
+            }
+        }
+        job.join()
+    }
+
+}
+
+fun c4(){
+    runBlocking {
+        val supervisor = SupervisorJob()
+        with(CoroutineScope(coroutineContext + supervisor)){
+            val job1 = launch {
+                println("job 1")
+                throw NullPointerException()
+            }
+            val job2 = launch {
+                println("job 2")
+                throw NullPointerException()
+            }
+            val job3 = launch {
+                println("job 3")
+                throw NullPointerException()
+            }
+        }
+    }
+}
+
+fun main() {
+    /*// c1: dung try cathc (Nhan loi) - throw( nem loi) launch
+    // c2: dung await(nhan loi) - throw (nem loi) async
+    //c1andc2()*/
+
+    /*// c3: dung handler
+    //c3()*/
+
+    /*Exception aggregation
+     c3 with multi exception: nhieu coroutine cung 1 loi -> handler chi xu ly loi dau tien, nhung cai
+     con lai da bi suppress
+     */
+    //c3vsMultiException()
+    //c4: dung supervisor job,
+    // (job thông thường : job cha bị hủy, sẽ hủy all job con bên trong) # (supervisor job bị hủy, job con vẫn sẽ chạy tiếp)
+
+
 }
